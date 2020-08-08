a18fbb235b178d6a061942375043875723a6856c
congcongfu <1246532936@qq.com>
2020-08-08 14:43:37 +0800

    delete

--- src/main/java/com/cong/rxjava/SchedulersTest.java
+++ src/main/java/com/cong/rxjava/SchedulersTest.java
@@ -1,4 +1,4 @@
-/**
+*
  * Company
  * Copyright (C) 2004-2018 All Rights Reserved.
  */
@@ -19,7 +19,6 @@ import io.reactivex.schedulers.Schedulers;
  * <p>文件名称: SchedulersTest.java</p>
  * <p>文件描述: </p>
  * <p>版权所有: 版权所有(C)2016-2099</p>
- * <p>公   司: 优行科技</p>
  * <p>内容摘要: </p>
  * <p>其他说明: </p>
  * <p>完成日期: @Date 2018年06月03日</p>
